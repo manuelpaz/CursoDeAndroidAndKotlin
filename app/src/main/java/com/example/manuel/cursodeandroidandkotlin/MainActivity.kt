507984ae730bb4249059368320d3b822578dadff
Manuel Paz Robles <manuelpazrobles@gmail.com>
2018-09-10 22:38:02 -0500

    Se agregan variables para ser utilizadas en los componentes antes creadas así como se agrego la interfazTextWatcher la cual implenta funciones que son los eventos: afterTextChanged, beforeTextChanged y onTextChanged en esta ultima se agrego el componente Toast  esto para mostrar como al cambiar el valor en los componentes Text se ejecuta este componente. El cambio en está actualización del curso es el resultado de los videos 4 y 5 del curso "Curso de Android con Kotlin desde cero" de Alex Pagoada.

--- app/src/main/java/com/example/manuel/cursodeandroidandkotlin/MainActivity.kt
+++ app/src/main/java/com/example/manuel/cursodeandroidandkotlin/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.manuel.cursodeandroidandkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextWatcher {
+    override fun afterTextChanged(s: Editable?) {
+
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        Toast.makeText(this, s.toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    private var editName: EditText? = null
+    private var editAge: EditText? = null
+    private var name: String? = null
+    private var age = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editName = findViewById(R.id.editText_Name)
+        editAge = findViewById(R.id.editText_Age)
+
+        editName?.addTextChangedListener(this)
+        editAge?.addTextChangedListener(this)
+
     }
 }
